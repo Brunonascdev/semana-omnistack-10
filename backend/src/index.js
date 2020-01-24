0f91acc27fcc7490d86d9117e99be02cc60da0f9
Bruno Nascimento <51379924+Brunonascdev@users.noreply.github.com>
2020-01-24 09:04:42 -0300

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,7 +11,7 @@ const server = http.Server(app);
 
 setupWebsocket(server);
 
-mongoose.connect('mongodb://bruno:bruno@cluster0-shard-00-00-xbcni.mongodb.net:27017,cluster0-shard-00-01-xbcni.mongodb.net:27017,cluster0-shard-00-02-xbcni.mongodb.net:27017/week10?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority', {
+mongoose.connect('linkdoseumongo', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
